6bf2f3a558e685341806e62e0b45fc9638647f32
Willy De Keyser <spring.boot.programming@gmail.com>
2025-09-02 16:27:07 +0200

    First commit

---
+++ src/main/java/willydekeyser/springbootdemo/controller/Hello_Controller.java
@@ -0,0 +1,13 @@
+package willydekeyser.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello_Controller {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/willydekeyser/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
